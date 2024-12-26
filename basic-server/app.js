8361986323bfc47c52adc04ee07e3b2804974e9e
Priya Aggarwal <priyaaggarwal24@gmail.com>
2024-12-26 17:27:11 +0530

    modules 1

--- basic-server/app.js
+++ basic-server/app.js
@@ -1,5 +1,6 @@
 const http = require('http');
-const fs = require('fs')
+const route = require('./route.js');
+const requestHandler = require('./route.js');
 
 /*
 Plain function
@@ -25,33 +26,6 @@ Arrow function
 }
 */
 
-const server = http.createServer((request, response)=> {
-    if(request.url === '/' && request.method === 'GET') {
-        response.write('<html>');
-        response.write('<head><title>A form</title></head>');
-        response.write('<body><form action="/message" method="POST"><input type="text" name="message"/> <button type="submit">Send</button></form></body>');
-        response.write('</html>');
-        return response.end();
-    } else if (request.url === '/message' && request.method === 'POST') {
-        const body = []
-        request.on('data', (chunk) => {
-            body.push(chunk);
-        })
-        return request.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            fs.writeFileSync('./basic-server/message.txt', message)
-            response.statusCode = 302
-            response.setHeader('Location', '/')
-            response.end();
-        })
-    } else {
-        response.write('<html>');
-        response.write('<head><title>Unhandled route</title></head>');
-        response.write('<body><h1>Unhandled route</h1></body>');
-        response.write('</html>');
-        return response.end();
-    }
-});
+const server = http.createServer(route);
 
 server.listen(3000);
---
+++ basic-server/route.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+
+function requestHandler(request, response) {
+    if(request.url === '/' && request.method === 'GET') {
+        response.write('<html>');
+        response.write('<head><title>A form</title></head>');
+        response.write('<body><form action="/message" method="POST"><input type="text" name="message"/> <button type="submit">Send</button></form></body>');
+        response.write('</html>');
+        return response.end();
+    } else if (request.url === '/message' && request.method === 'POST') {
+        const body = []
+        request.on('data', (chunk) => {
+            body.push(chunk);
+        })
+        return request.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('./basic-server/message.txt', message)
+            response.statusCode = 302
+            response.setHeader('Location', '/')
+            response.end();
+        })
+    } else {
+        response.write('<html>');
+        response.write('<head><title>Unhandled route</title></head>');
+        response.write('<body><h1>Unhandled route</h1></body>');
+        response.write('</html>');
+        return response.end();
+    }
+}
+
+module.exports = requestHandler
